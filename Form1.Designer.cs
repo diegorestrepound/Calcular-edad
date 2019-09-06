ec9ed5783475c9889b7ea5d7a8378506ad703b99
Diego Restrepo <diegorestrepound@gmail.com>
2019-09-06 16:58:43 -0500

    un programa en el cual se utiliza un cuadro de datos de tiempo para calcular la edad de una persona

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace edad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbledad = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnCalculaEdad = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "fecha de nacimiento:";
+            // 
+            // lbledad
+            // 
+            this.lbledad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbledad.Location = new System.Drawing.Point(150, 146);
+            this.lbledad.Name = "lbledad";
+            this.lbledad.Size = new System.Drawing.Size(35, 13);
+            this.lbledad.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad:";
+            // 
+            // FechaNacimiento
+            // 
+            this.FechaNacimiento.Location = new System.Drawing.Point(206, 106);
+            this.FechaNacimiento.Name = "FechaNacimiento";
+            this.FechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.FechaNacimiento.TabIndex = 3;
+            // 
+            // btnCalculaEdad
+            // 
+            this.btnCalculaEdad.Location = new System.Drawing.Point(38, 197);
+            this.btnCalculaEdad.Name = "btnCalculaEdad";
+            this.btnCalculaEdad.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculaEdad.TabIndex = 4;
+            this.btnCalculaEdad.Text = "Calcular Edad";
+            this.btnCalculaEdad.UseVisualStyleBackColor = true;
+            this.btnCalculaEdad.Click += new System.EventHandler(this.btnCalculaEdad_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(331, 197);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(183, 197);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalculaEdad);
+            this.Controls.Add(this.FechaNacimiento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbledad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbledad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker FechaNacimiento;
+        private System.Windows.Forms.Button btnCalculaEdad;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace edad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lbledad.Text = "";
+        }
+
+        private void btnCalculaEdad_Click(object sender, EventArgs e)
+        {
+            DateTime Fechanac = FechaNacimiento.Value;
+            int años = System.DateTime.Now.Year - Fechanac.Year;
+            if (System.DateTime.Now.Subtract(Fechanac.AddYears(años)).TotalDays < 0)
+                lbledad.Text = Convert.ToString(años - 1);
+            else
+                lbledad.Text = Convert.ToString(años);
+
+        }
+    }
+}
